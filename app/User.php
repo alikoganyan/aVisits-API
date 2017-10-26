30efacfefca4c9e88a69973bef8005d3e274ccfe
Alik Ohanyan <alik.oganyan@yandex.ru>
2017-10-26 11:20:01 +0300

    User phone number save fix

--- app/User.php
+++ app/User.php
@@ -15,7 +15,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'name', 'email', 'phone', 'password',
     ];
 
     /**
@@ -29,6 +29,6 @@ class User extends Authenticatable
 
     public function chains()
     {
-        return $this->hasMany('App\Models\Chain','user_id','id');
+        return $this->hasMany('App\Models\Chain', 'user_id', 'id');
     }
 }
