b43d65d565ff17d02e238bf6600dbcb8efeb18ea
Alik Ohanyan <alik.oganyan@yandex.ru>
2017-10-27 10:20:44 +0300

    employee migration

--- database/migrations/2017_10_25_124455_change_employees_table.php
+++ database/migrations/2017_10_25_124455_change_employees_table.php
@@ -14,21 +14,21 @@ class ChangeEmployeesTable extends Migration
     public function up()
     {
         Schema::table('employees', function (Blueprint $table) {
-            $table->dropColumn('card_number');
-            $table->dropColumn('card_number_optional');
-            $table->dropColumn('deposit');
-            $table->dropColumn('bonuses');
-            $table->dropColumn('invoice_sum');
-            $table->dropColumn('position_id');
-            $table->dropColumn('public_position');
-            $table->dropColumn('phone_2');
-            $table->dropColumn('employment_date');
-            $table->dropColumn('dismissed');
-            $table->dropColumn('dismissed_date');
-            $table->dropColumn('displayed_in_records');
-            $table->dropColumn('available_for_online_recording');
-            $table->dropColumn('access_profile_id');
-            $table->dropForeign('employees_position_id_foreign');
+//            $table->dropColumn('card_number');
+//            $table->dropColumn('card_number_optional');
+//            $table->dropColumn('deposit');
+//            $table->dropColumn('bonuses');
+//            $table->dropColumn('invoice_sum');
+//            $table->dropColumn('position_id');
+//            $table->dropColumn('public_position');
+//            $table->dropColumn('phone_2');
+//            $table->dropColumn('employment_date');
+//            $table->dropColumn('dismissed');
+//            $table->dropColumn('dismissed_date');
+//            $table->dropColumn('displayed_in_records');
+//            $table->dropColumn('available_for_online_recording');
+//            $table->dropColumn('access_profile_id');
+//            $table->dropForeign('employees_position_id_foreign');
         });
     }
 
