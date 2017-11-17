0f9b57080307b4fc96c9676a420ad88cb53d2fc5
Alik Ohanyan <alik.oganyan@yandex.ru>
2017-11-17 15:01:16 +0300

    debug mail

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -146,7 +146,7 @@ class UserController extends Controller
                 $headers = "MIME-Version: 1.0" . "\r\n";
                 $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
                 $headers .= 'From: aVisits <noreply@avisits.com>' . "\r\n";
-                mail($request->input('email'), 'Reset Password', 'http://avisits.com/#/auth/reset-password?token=' . $token, $headers);
+                mail($request->input('email'), 'Reset Password', '<p>dasdasd</p>http://avisits.com/#/auth/reset-password?token=' . $token, $headers);
             }
             return response()->json(['success' => true, 'errors' => []], 200);
         }
