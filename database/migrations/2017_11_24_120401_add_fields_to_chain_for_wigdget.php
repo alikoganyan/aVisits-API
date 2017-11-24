a905cb88d5624a3bdfe4e2597c557fcbaaf43056
Alik Ohanyan <alik.oganyan@yandex.ru>
2017-11-24 17:26:06 +0400

    Chain table add new fields for widget

---
+++ database/migrations/2017_11_24_120401_add_fields_to_chain_for_wigdget.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddFieldsToChainForWigdget extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('chains', function (Blueprint $table) {
+            $table->string('w_color',255)->nullable()->after('user_id');
+            $table->tinyInteger('w_let_check_steps',false,true)->nullable()->after('w_color');
+            $table->string('w_steps_g',255)->nullable()->after('w_let_check_steps');
+            $table->string('w_steps_service',255)->nullable()->after('w_steps_g');
+            $table->string('w_steps_employee',255)->nullable()->after('w_steps_service');
+            $table->enum('w_contact_step',['at_first','after_address','at_the_end'])->nullable()->after('w_steps_employee');
+            $table->tinyInteger('w_to_group_category',false,true)->nullable()->after('w_contact_step');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('chains', function (Blueprint $table) {
+            //
+        });
+    }
+}
